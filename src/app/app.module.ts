da759285ed2dca89822e4ea3e17761e92b5d2083
vvieira <vvieira@DCCNOTERENT09>
2021-08-09 16:02:51 -0300

    estudos angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,45 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { InputBindingComponent } from './input-binding/input-binding.component';
+import { ClientComponent } from './input-binding/client/client.component';
+import { EventComponent } from './event/event.component';
+import { ChildItemComponent } from './event/child-item/child-item.component';
+import { MatButtonModule, MatCardModule, MatFormFieldModule, MatIconModule, MatInputModule, MatProgressBar, MatProgressBarModule } from '@angular/material';
+import { ClientsComponent } from './clients/clients.component';
+import { FormsModule } from '@angular/forms';
+import { ItemClientComponent } from './clients/item-client/item-client.component';
+import { ParentChildComponent } from './parent-child/parent-child.component';
+import { TimerComponent } from './parent-child/timer/timer.component';
+import { OnChangesComponent } from './on-changes/on-changes.component';
+import { NameChangesComponent } from './on-changes/name-changes/name-changes.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputBindingComponent,
+    ClientComponent,
+    EventComponent,
+    ChildItemComponent,
+    ClientsComponent,
+    ItemClientComponent,
+    ParentChildComponent,
+    TimerComponent,
+    OnChangesComponent,
+    NameChangesComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule, 
+    MatProgressBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
